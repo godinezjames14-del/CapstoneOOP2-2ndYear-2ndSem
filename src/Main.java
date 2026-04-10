bfdd6b7f6c731698aced89f84bfb702e920d3a2f
James Godinez <godinezjames14@gmail.com>
2026-04-10 23:10:34 +0800

    Commit Message

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(Main.class.getResource("mainmenu-view.fxml"));
+        Scene scene = new Scene(loader.load(), 1440, 1024);
+        stage.setTitle("Main Menu");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
---
+++ src/MainMenuController.java
@@ -0,0 +1,39 @@
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent; // CRITICAL: Must be this import
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainMenuController implements Initializable {
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        // Leave empty for now
+    }
+
+    @FXML
+    private void RegisterScreen(MouseEvent event) { // Use MouseEvent, not ActionEvent
+        try {
+            // Load the FXML for the registration screen
+            Parent registerRoot = FXMLLoader.load(getClass().getResource("register-view.fxml"));
+
+            // Get the current window (Stage)
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+            // Swap the scene
+            stage.setScene(new Scene(registerRoot));
+            stage.show();
+
+        } catch (IOException e) {
+            System.err.println("Error: Could not find register-view.fxml");
+            e.printStackTrace();
+        }
+    }
+}
